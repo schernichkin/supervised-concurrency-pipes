dd4bea1defb83972e3c3f913093705ddb52d1cff
Stanislav Chernichkin <schernichkin@gmail.com>
2015-03-05 05:33:12 -0800

    added IO synch in sample program

--- samples/token-stealing/Main.hs
+++ samples/token-stealing/Main.hs
@@ -3,24 +3,25 @@
 
 module Main where
 
-import           Control.Concurrent          (ThreadId)
+import           Control.Concurrent          (ThreadId, newMVar, withMVar)
 import           Control.Monad
 import           Control.Monad.Base
 import           Pipes
 import           Pipes.Concurrent.Supervised
+-- import           Control.Concurrent.Lifted (newMVar, withMVar)
 
 data Token = Token String Int deriving ( Show )
 
-thief :: (MonadSupervisor m, MonadIO m) => Pipe Token Token m ()
-thief = do
+thief :: (MonadSupervisor m, MonadIO m) => (String -> IO ()) -> Pipe Token Token m ()
+thief printer = do
     token@(Token name count)  <- await
     (Just threadName)  <- lift $ getThreadName
-    liftIO $ print $ threadName ++ " snitched " ++ (show token)
+    liftIO $ printer $ threadName ++ " snitched " ++ (show token)
     when (count > 0) $ yield $ Token name (pred count)
-    thief
+    thief printer 
 
-spawnWithTracer :: (MonadSupervisor m) => String -> m () -> m ()
-spawnWithTracer name action = do
+spawnWithTracer :: (MonadSupervisor m) => String -> (String -> IO ()) -> m () -> m ()
+spawnWithTracer name printer action = do
     threadId <- spawnNamed name action
     void $ spawnNamed ("Tracer for " ++ name) $ tracer threadId Unstarted
     where
@@ -30,15 +31,17 @@ spawnWithTracer name action = do
             case maybeInfo of
                 Nothing -> return ()
                 Just info -> do
-                    liftBase $ print info
+                    liftBase $ printer (show info)
                     tracer threadId (_threadState info)
 
 main :: IO ()
 main = do
     runSupervisorT $ do
+        consoleLock <- lift $ newMVar ()
+        let printLn = withMVar consoleLock . const . putStrLn
         void $ setThreadName "main"
         (output, input) <- newChannel Unbounded
-        forM_ [1..10 :: Int] $ \i -> spawnWithTracer ("thief " ++ (show i))  $ runEffect (fromInput input >-> thief >-> toOutput output)
+        forM_ [1..10 :: Int] $ \i -> spawnWithTracer ("thief " ++ (show i)) printLn $ runEffect (fromInput input >-> (thief printLn) >-> toOutput output)
         forM_ [1..3 :: Int]  $ \i -> send output $ Token ("token " ++ (show i)) $ 6
         waitTill NoRunningThreads
     putStrLn "All threads terminated"
